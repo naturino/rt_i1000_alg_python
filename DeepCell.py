643cc53f6a8253b648646294857ad54ef161a4d6
naturino <naturino_mu@163.com>
2023-09-04 10:30:08 +0800

    modify DeepCell.py

--- DeepCell.py
+++ DeepCell.py
@@ -1,17 +1,15 @@
-from i1000_alg import *
-import loger
-import os
-import socket_server
 import torch
 import threading
 from mmengine.fileio import dump
-from model import FileOperate
 
+import loger
+import socket_server
+from i1000_alg import *
 
 class DeepCell():
     def __init__(self, config_path):
         # 读取配置文件
-        config = self.__read_config(config_path)
+        config = self._read_config(config_path)
 
         # 初始化配置文件
         config_json = dump(config, file_format='json', indent=4)
@@ -20,6 +18,7 @@ class DeepCell():
             current_path = os.getcwd()
             config['TEMP_PATH'] = os.path.join(current_path, 'alg', 'temp').replace('\\', '/')
             temp_path = config['TEMP_PATH']
+
         self.file_operate = FileOperate()
         self.file_operate.del_dir(temp_path)
 
@@ -27,9 +26,6 @@ class DeepCell():
         self.logger = loger.Logger('alg/log')
         self.logger.info(f"Config Info: {config_json}")
         self.logger.info(f"DeepCell Start")
-
-        gpu_info = self.__get_gpu_info()
-        self.logger.info(f"Find GPU: {gpu_info}")
 
         # 初始化变量
         self.empty = config['EMPTY']
@@ -39,41 +35,35 @@ class DeepCell():
         self.run_lock = threading.Lock()
 
         # 启动模型
-        self.start_model(config)
+        model_state = self._start_model(config)
 
-        # 开启通信
-        self.start_socket()
+        if model_state:
+            # 开启通信
+            self._start_socket()
 
-    def __read_config(self, path):
-        dst = {}
-        exec(open(path).read(), dst)
-        del_key = '__builtins__'
-        if del_key in dst.keys():
-            del dst[del_key]
-        return dst
+    def run(self, info):
+        with self.run_lock:
+            dst = self.empty
+            task, img_path = self._get_task(info)
 
-    def __get_gpu_info(self):
-        gpu_info = ''
-        if torch.cuda.is_available():
-            gpu = torch.cuda.get_device_properties(0)
-            gpu_info = gpu.name
-        return gpu_info
+            if not os.path.isfile(img_path):
+                self.logger.info(f'{img_path} is not file')
+                return dst
+            if not self.file_operate.is_type(img_path, self.file_type):
+                self.logger.info(f'{img_path} is not in {self.file_type}')
+                return dst
+            if task not in self.task_model.keys():
+                self.logger.info(f"Dont't find {task} model")
+                return dst
 
-    def model_test(self):
-        self.run('alg/assets/img/demo1/1_7.png*0')
-        self.run('alg/assets/img/demo1/1_28.png*1')
-        self.run('alg/assets/img/demo3.png*2')
-        self.run('alg/assets/img/demo2.jpg')
-        self.logger.info(f"Model Test Successful")
+            dst = self.task_model[task].run(img_path)
 
-    def model_debug(self):
-        floder = 'F:/adam/i1000/adk/adk_unet'
-        for root, _, file_list in os.walk(floder):
-            for file in file_list:
-                path = os.path.join(root, file) + '*2'
-                self.run(path)
+            if type(dst) != str:
+                dst = json.dumps(dst, indent=4)
 
-    def start_model(self,config):
+            return dst
+
+    def _start_model(self,config):
 
         self.task_model = {}
 
@@ -83,13 +73,16 @@ class DeepCell():
         if self.jsons_path != '':
             self.only_analyse_json = True
 
-        self.test = config['TEST_INIT']
+        gpu_is_available = False
+        gpu_info = self._get_gpu_info()
+        self.logger.info(f"Find GPU: {gpu_info}")
+        if gpu_info == 'NVIDIA GeForce RTX 3060':
+            gpu_is_available = True
+        is_cpu = config['DEVICE'] == 'cpu'
 
-        self.debug = config['DEBUG']
+        self.task_model['100XJson'] = I1000AnalyseLabelmeJsons(config, self.logger)
 
-        if self.only_analyse_json:
-            self.task_model['100XJson'] = I1000AnalyseLabelmeJsons(config, self.logger)
-        else:
+        if gpu_is_available or is_cpu:
             # 初始化算法模型
             self.task_model['100XFlip'] = I1000FlipModel(config, self.logger)
             self.task_model['100XCls'] = I1000ClsModel(config, self.logger, self.task_model['100XFlip'])
@@ -99,14 +92,22 @@ class DeepCell():
                                                                self.task_model['100XCls'], self.task_model['100XFlip'])
             self.logger.info(f"Model Load Successful")
 
+        else:
+            self.logger.info(f"Don't find device...")
+            return False
+
         # 测试模型
-        if self.test:
-            self.model_test()
+        is_test = config['TEST_INIT']
+        if is_test:
+            self._model_test()
 
-        if self.debug:
-            self.model_debug()
+        debug = config['DEBUG']
+        if debug:
+            self._model_debug()
 
-    def start_socket(self,config):
+        return True
+
+    def _start_socket(self,config):
         self.host = config['HOST']
         self.port1 = config['PORT1']
         self.port2 = config['PORT2']
@@ -136,7 +137,36 @@ class DeepCell():
             self.server1.stop()
             self.server2.stop()
 
-    def get_task(self,info):
+    def _read_config(self, path):
+        dst = {}
+        exec(open(path).read(), dst)
+        del_key = '__builtins__'
+        if del_key in dst.keys():
+            del dst[del_key]
+        return dst
+
+    def _get_gpu_info(self):
+        gpu_info = ''
+        if torch.cuda.is_available():
+            gpu = torch.cuda.get_device_properties(0)
+            gpu_info = gpu.name
+        return gpu_info
+
+    def _model_test(self):
+        self.run('alg/assets/img/demo1/1_7.png*0')
+        self.run('alg/assets/img/demo1/1_28.png*1')
+        self.run('alg/assets/img/demo3.png*2')
+        self.run('alg/assets/img/demo2.jpg')
+        self.logger.info(f"Model Test Successful")
+
+    def _model_debug(self):
+        floder = 'F:/adam/i1000/adk/adk_unet'
+        for root, _, file_list in os.walk(floder):
+            for file in file_list:
+                path = os.path.join(root, file) + '*2'
+                self.run(path)
+
+    def _get_task(self,info):
         dst = ''
         info_list = info.split('*')
         image_path = info_list[0]
@@ -160,28 +190,7 @@ class DeepCell():
 
         return dst, image_path
 
-    def run(self, info):
-        with self.run_lock:
-            dst = self.empty
-            task, img_path = self.get_task(info)
-
-            if not os.path.isfile(img_path):
-                self.logger.info(f'{img_path} is not file')
-                return dst
-            if not self.file_operate.is_type(img_path, self.file_type):
-                self.logger.info(f'{img_path} is not in {self.file_type}')
-                return dst
-            if task not in self.task_model.keys():
-                self.logger.info(f"Dont't find {task} model")
-                return dst
-
-            dst = self.task_model[task].run(img_path)
-
-            if type(dst) != str:
-                dst = json.dumps(dst, indent=4)
-
-            return dst
-
 if __name__ == "__main__":
-    config_path = 'alg/assets/config.py'
-    server = DeepCell(config_path)
+    deepcell_path = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(deepcell_path, 'alg/assets/config.py')
+    DeepCell(config_path)
